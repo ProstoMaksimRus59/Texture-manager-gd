15febc4ae49d2e1c4dc021ab1fb965e95ab98ae8
ProstoMaksimRus59 <102693495+ProstoMaksimRus59@users.noreply.github.com>
2022-03-31 16:47:55 +0500

    0.3beta
    You can choose another place of the game without rebuilding.

--- tmgd.py
+++ tmgd.py
@@ -1,22 +1,36 @@
+#Shit code. :)
 import zipfile, os
-Gamefolder = "C:\\Program Files (x86)\\Steam\\steamapps\\common\\Geometry Dash"
 files = os.listdir()
 start = 0
-for ls in files:
+creat = 0
+for ls in files:    #Check for the settings file.
+    if ls == "settings.set":
+        creat = 1
+        set = open("settings.set", 'r')
+        Gamefolder = set.read()
+        set.close
+if creat == 0:  #Creating a settings file.
+    Way = input("Path to the game.\nFor example: C:\\yy\\xx\\Geometry Dash\n>")
+    set = open("settings.set", 'w')
+    set.write(Way)
+    set.close()
+    Gamefolder = Way
+for ls in files:    #Check for archive.
     if ls == "Officialtexture.zip":
         start = 1
-if start == 1:
-    print("Texture manager gd \"v0.2beta\"")
+if start == 1:  #If there is an archive.
+    print("Texture manager gd \"v0.3beta\"")
     print("/help for reference.")
-if start == 0:
+if start == 0:  #If there is no archive.
     print("No Archive: OfficialTexture.zip")
     input("")
-while start == 1:
+while start == 1:       #Command line.
     COM = input("tmgd> ")
     if COM == "/help":
         print("/return_Set Officialtexture gd.")
         print("/install_Set your texture.")
         print("/exit_Turn off the program.")
+        print("/reset_Reset settings.")
     if COM == "/return":
         z = zipfile.ZipFile("Officialtexture.zip", 'r')
         z.extractall(Gamefolder)
@@ -27,4 +41,8 @@ while start == 1:
         z.extractall(Gamefolder)
         print("done.")
     if COM == "/exit":
+        exit()
+    if COM == "/reset":
+        os. remove("settings.set")
+        input("Settings are dropped.\nThe program will be turned off.")
         exit()

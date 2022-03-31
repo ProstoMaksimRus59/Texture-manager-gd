4f87354a500d3cdd6e17da8903cc7bbe36c06ab2
ProstoMaksimRus59 <102693495+ProstoMaksimRus59@users.noreply.github.com>
2022-03-31 11:01:13 +0500

    v0.2beta
    Protection from fool

--- tmgd.py
+++ tmgd.py
@@ -1,20 +1,30 @@
-import zipfile
-print("Texture manager gd \"v0.1beta\"")
-print("/help for reference.")
-while 1 == 1:
+import zipfile, os
+Gamefolder = "C:\\Program Files (x86)\\Steam\\steamapps\\common\\Geometry Dash"
+files = os.listdir()
+start = 0
+for ls in files:
+    if ls == "Officialtexture.zip":
+        start = 1
+if start == 1:
+    print("Texture manager gd \"v0.2beta\"")
+    print("/help for reference.")
+if start == 0:
+    print("No Archive: OfficialTexture.zip")
+    input("")
+while start == 1:
     COM = input("tmgd> ")
     if COM == "/help":
-        print("/return_Officialtexture gd.")
+        print("/return_Set Officialtexture gd.")
         print("/install_Set your texture.")
         print("/exit_Turn off the program.")
     if COM == "/return":
         z = zipfile.ZipFile("Officialtexture.zip", 'r')
-        z.extractall("C:\\Program Files (x86)\\Steam\\steamapps\\common\\Geometry Dash")
+        z.extractall(Gamefolder)
         print("done.")
     if COM == "/install":
         TP = input("file> ")
         z = zipfile.ZipFile(TP, 'r')
-        z.extractall("C:\\Program Files (x86)\\Steam\\steamapps\\common\\Geometry Dash")
+        z.extractall(Gamefolder)
         print("done.")
     if COM == "/exit":
         exit()
